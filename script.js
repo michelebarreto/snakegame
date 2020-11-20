1f6666bfe5e7644edbacd4d2d0290c9751c2b26e
Michele Barreto <michelebarreto1@hotmail.com>
2020-11-20 00:00:24 -0300

    animação da cobra

--- script.js
+++ script.js
@@ -56,6 +56,18 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y=0;
     if(snake[0].y < 0 && direction =="up") snake[0].y = 16 * box;
 
+//criando for para condição de quando a cobra se chocar terminar o jogo
+for(i=1; i < snake.length; i++){
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+        clearInterval(jogo);
+        alert("Game Over :( ");
+
+    }
+}
+
+
+
+
 //chamando as funções
 criarBG();
 criarCobrinha();
